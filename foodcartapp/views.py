bdc99b03f8ef0a3bfe4c7196513ce9c4ed4fe4f5
AlexRikka <rikka.gold@mail.ru>
2025-01-14 20:55:46 +0400

    Send order data to frontend

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,17 +1,16 @@
 import phonenumbers
 
-from django.http import JsonResponse, Http404, HttpResponse
+from django.http import JsonResponse, Http404
 from django.shortcuts import get_object_or_404
 from django.templatetags.static import static
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
 from rest_framework.serializers import ValidationError
-from rest_framework.serializers import ModelSerializer, ListField
+from rest_framework.serializers import ModelSerializer, Serializer
+from rest_framework.serializers import ListField, IntegerField, CharField
 
 from .models import Product, Order, OrderItem
-#from .serializers import OrderSerializer
 
 
 def banners_list_api(request):
@@ -66,87 +65,63 @@ def product_list_api(request):
     })
 
 
+class ProductsSerializer(Serializer):
+    product = IntegerField()
+    quantity = IntegerField()
+
+
 class OrderSerializer(ModelSerializer):
-    products = ListField(allow_empty=False)
+    products = ListField(child=ProductsSerializer(),
+                         allow_empty=False, write_only=True)
+    phonenumber = CharField()
 
     class Meta:
         model = Order
-        fields = ['firstname', 'lastname', 'address', 'products']
+        fields = ['id', 'firstname', 'lastname',
+                  'address', 'phonenumber', 'products']
 
-    def phonenumber_type(self, value):
+    def validate_phonenumber(self, value):
         if not phonenumbers.parse(value, 'RU'):
             raise ValidationError('Wrong value')
         phonenumber = phonenumbers.parse(value, 'RU')
         if not phonenumbers.is_valid_number(phonenumber):
             raise ValidationError('Wrong format')
-        phonenumber = phonenumbers.format_number(
+        phonenumber_formatted = phonenumbers.format_number(
             phonenumber, phonenumbers.PhoneNumberFormat.E164)
 
-        return phonenumber
+        return phonenumber_formatted
 
 
-# @api_view(['POST'])
-# def register_order(request):
-#     err_content = None
-#     product_id = None
+@api_view(['POST'])
+def register_order(request):
+    err_content = None
+    product_id = None
 
-#     data = request.data
-#     serializer = OrderSerializer(data=data)
-#     serializer.is_valid(raise_exception=True)
+    data = request.data
+    serializer = OrderSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
 
-#     try:
-#         new_order = Order.objects.create(firstname=data['firstname'],
-#                                         lastname=data['lastname'],
-#                                         phonenumber=data['phonenumber'],
-#                                         address=data['address'])
+    try:
+        new_order = Order.objects.create(
+            firstname=serializer.validated_data['firstname'],
+            lastname=serializer.validated_data['lastname'],
+            phonenumber=serializer.validated_data['phonenumber'],
+            address=serializer.validated_data['address'])
 
-#         for product in data['products']:
-#             product_id = product['product']
-#             order_item = get_object_or_404(
-#                 Product, id=product_id)
-#             OrderItem.objects.create(order=new_order,
-#                                     product=order_item,
-#                                     quantity=product['quantity'])
+        for product in data['products']:
+            product_id = product['product']
+            order_item = get_object_or_404(
+                Product, id=product_id)
+            OrderItem.objects.create(order=new_order,
+                                     product=order_item,
+                                     quantity=product['quantity'])
 
-#     except Http404:
-#         err_content = {
-#             'error': f'products: invalid id {product_id}'} \
-#             if not err_content else err_content
-#         return Response(err_content, status=status.HTTP_404_NOT_FOUND)
-    
-#     return JsonResponse(data)
+    except Http404:
+        err_content = {
+            'error': f'products: invalid id {product_id}'} \
+            if not err_content else err_content
+        return Response(err_content, status=status.HTTP_404_NOT_FOUND)
 
+    new_order_serialized = OrderSerializer(new_order)
 
-def order_list_api(request):
-    if request.method == 'GET':
-        orders = Order.objects.all()
-        serializer = OrderSerializer(orders, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = OrderSerializer(data=data)
-        if serializer.is_valid():
-            #serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-
-def order_detail_api(request, pk):
-    order = get_object_or_404(Order, pk=pk)
-
-    if request.method == 'GET':
-        serializer = OrderSerializer(order)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = OrderSerializer(order, data=data)
-        if serializer.is_valid():
-            #serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        order.delete()
-        return HttpResponse(status=204)
+    return Response(new_order_serialized.data)
