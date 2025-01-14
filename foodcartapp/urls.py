a23681ac16640eb264db04a5e9f5c616e39a0445
AlexRikka <rikka.gold@mail.ru>
2025-01-14 21:01:21 +0400

    remove extra code

--- foodcartapp/urls.py
+++ foodcartapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views import product_list_api, banners_list_api , register_order
-#from .views import order_detail_api, order_list_api
+from .views import product_list_api, banners_list_api, register_order
 
 app_name = "foodcartapp"
 
@@ -9,6 +8,4 @@ urlpatterns = [
     path('products/', product_list_api),
     path('banners/', banners_list_api),
     path('order/', register_order),
-    #path('orders/', order_list_api),
-    #path('orders/<int:pk>/', order_detail_api),
 ]
